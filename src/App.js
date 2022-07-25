eabb071f8a0ac5264eeceb52f016121140d54dd5
chwangmin <44563065+chwangmin@users.noreply.github.com>
2022-07-25 17:22:16 +0900

    fix router

--- src/App.js
+++ src/App.js
@@ -1,35 +1,42 @@
-import React, { Component } from 'react';
-import './App.css';
-import Login from "./routes/Login"
-import Signup from "./routes/Signup"
-import Home from "./routes/Home"
-import Infodisease from "./routes/Infodisease"
-import Question from "./routes/Question"
-import Answer from "./routes/Answer"
-import Scrap from "./routes/Scrap"
-import Qna from "./routes/Qna"
-import Judgment from "./routes/Judgment"
+import React, { Component } from "react";
+import "./App.css";
+import Login from "./routes/Login";
+import Signup from "./routes/Signup";
+import Home from "./routes/Home";
+import Infodisease from "./routes/Infodisease";
+import Question from "./routes/Question";
+import Answer from "./routes/Answer";
+import Scrap from "./routes/Scrap";
+import Qna from "./routes/Qna";
+import Judgment from "./routes/Judgment";
 import "bootstrap/dist/css/bootstrap.min.css";
-import {Routes, Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
-function App() {  
+function App() {
   return (
     <div>
-        <Routes>
-          <Route exact path="/" element={<Home />}></Route>
-          <Route path="/login" element={<Login />}></Route>
-          <Route path="/signup" element={<Signup />}></Route>
-          <Route path="/scrap" element={<Scrap />}></Route>
-          <Route path="/judgment" element={<Judgment />}></Route>
-          <Route path="/infodisease/:diseaseid" element={<Infodisease />}></Route>
-          <Route path="/infodisease/:diseaseid/question" element={<Question />}></Route> 
-          <Route path="/infodisease/:diseaseid/qna/:qnaid" element={<Qna />}></Route>                
-          <Route path="/infodisease/:diseaseid/qna/:qnaid/answer" element={<Answer />}></Route> 
-        </Routes>
+      <Routes>
+        <Route exact path="/" element={<Home />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/signup" element={<Signup />}></Route>
+        <Route path="/scrap" element={<Scrap />}></Route>
+        <Route path="/judgment" element={<Judgment />}></Route>
+        <Route path="/infodisease/:diseaseid" element={<Infodisease />}></Route>
+        <Route
+          path="/infodisease/:diseaseid/question"
+          element={<Question />}
+        ></Route>
+        <Route
+          path="/infodisease/:diseaseid/qna/:qnaid"
+          element={<Qna />}
+        ></Route>
+        <Route
+          path="/infodisease/:diseaseid/qna/:qnaid/answer"
+          element={<Answer />}
+        ></Route>
+      </Routes>
     </div>
   );
-
 }
-
 
 export default App;
